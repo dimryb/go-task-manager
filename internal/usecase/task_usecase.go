4778a523392baea3836a285f6bed6e91ee760dc8
dimryb <dimryb@bk.ru>
2025-01-28 00:04:30 +0700

    Создал интерфейс юзкейсов

---
+++ internal/usecase/task_usecase.go
@@ -0,0 +1,13 @@
+package usecase
+
+import (
+	"go-task-manager/internal/domain"
+)
+
+type TaskUseCase interface {
+	CreateTask(task domain.Task) error
+	GetTasks() ([]domain.Task, error)
+	GetTaskByID(id uint) (domain.Task, error)
+	UpdateTask(task domain.Task) error
+	DeleteTask(id uint) error
+}
