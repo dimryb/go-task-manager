cd6f2bcdd31ea16b411f93cc8a6b727dcc4f2e32
dimryb <dimryb@bk.ru>
2025-01-28 00:34:07 +0700

    Реализовал обработчик создания задачи

--- internal/interfaces/http/handler/task_handler.go
+++ internal/interfaces/http/handler/task_handler.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"go-task-manager/internal/domain"
 	"go-task-manager/internal/usecase"
 	"net/http"
 )
@@ -10,5 +12,16 @@ type TaskHandler struct {
 }
 
 func (h TaskHandler) CreateTask(writer http.ResponseWriter, request *http.Request) {
-	//todo: реализовать
+	var req domain.Task
+	if err := json.NewDecoder(request.Body).Decode(&req); err != nil {
+		http.Error(writer, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.TaskUseCase.CreateTask(req); err != nil {
+		http.Error(writer, "Failed to create task", http.StatusInternalServerError)
+		return
+	}
+
+	writer.WriteHeader(http.StatusCreated)
 }
